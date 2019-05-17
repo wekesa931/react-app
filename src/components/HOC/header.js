29bcd72f9abaaa831400846dfebe47d5803e4bba
bill wekesa <billwekesa@bills-MacBook-Pro.local>
2019-05-17 05:46:26 +0300

    Added Home page

---
+++ src/components/HOC/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return ( 
+        <div>
+            <h5>Questioner</h5>
+            <hr/>
+        </div>
+     );
+}
+ 
+export default Header;
