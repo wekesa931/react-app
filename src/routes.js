401593e7c2d4cfc6b40600efba4006c1c90cd7b9
bill wekesa <billwekesa@bills-MacBook-Pro.local>
2019-05-17 07:15:02 +0300

    added test configs

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,12 @@ import Layout from './components/HOC/layout';
 import Home from './components/home';
 import SignIn from './components/signin';
 import { Container } from 'reactstrap';
-// import Dashboard from './components/dashboard';
 
 const Routes = () => {
     return ( 
         <Container>
             <Layout>
-                <Route path="/auth/signup" exact component={Auths}/>
+                <Route path="/" exact component={Auths}/>
                 <Route path="/auth/signin" exact component={SignIn}/>
                 <Route path="/home" exact component={Home}/>
             </Layout>
